f2d67dbf75d2861652ff9d6a4e888b8edcd127ad
ero.carrera <ero.carrera@06aa9b79-7134-0410-ae7e-c1cd3e483e87>
2011-04-10 15:55:59 +0000

    pydot-1.0.25

---
+++ tags/pydot-1.0.25/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+try:
+    from distutils.core import setup
+except ImportError, excp:
+    from setuptools import setup
+    
+import pydot
+import os
+
+os.environ['COPY_EXTENDED_ATTRIBUTES_DISABLE'] = 'true'
+os.environ['COPYFILE_DISABLE'] = 'true'
+
+setup(	name = 'pydot',
+    version = pydot.__version__,
+    description = 'Python interface to Graphviz\'s Dot',
+    author = 'Ero Carrera',
+    author_email = 'ero@dkbza.org',
+    url = 'http://code.google.com/p/pydot/',
+    download_url = 'http://pydot.googlecode.com/files/pydot-%s.tar.gz' % pydot.__version__,
+    license = 'MIT',
+    keywords = 'graphviz dot graphs visualization',
+    platforms = ['any'],
+    classifiers =	['Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+    long_description = "\n".join(pydot.__doc__.split('\n')),
+    py_modules = ['pydot', 'dot_parser'],
+    install_requires = ['pyparsing', 'setuptools'],
+    data_files = [('.', ['LICENSE', 'README'])] )
