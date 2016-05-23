e26af21426fcf15955f7b11b348b14dc6bd5178c
Ioannis Filippidis <jfilippidis@gmail.com>
2016-05-23 03:27:32 -0700

    REL: unpin to `pyparsing >= 1.5.7`
    because `dot_parser.py` has been updated to not import
    any hidden or unused items that have been removed from
    `pyparsing >= 2.0.1`.

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setup(	name = 'pydot',
         'Topic :: Software Development :: Libraries :: Python Modules'],
     long_description = "\n".join(pydot.__doc__.split('\n')),
     py_modules = ['pydot', 'dot_parser'],
-    install_requires = ['pyparsing==1.5.7'],
+    install_requires = ['pyparsing>=1.5.7'],
     data_files = [('.', ['LICENSE', 'README'])] )
