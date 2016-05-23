6dff94b3f1dbcf68e5a83ce17a36b098c06d7c6b
Lance Hepler <nlhepler@gmail.com>
2016-05-23 03:26:22 -0700

    MAI: do not import hidden `pyparsing._noncomma`, instead reconstruct it
    because `_noncomma` has been removed in `pyparsing >= 2.0.1`.
    This change makes `pydot >= 1.1.0` compatible with `pyparsing >= 1.5.7`.

--- dot_parser.py
+++ dot_parser.py
@@ -25,7 +25,7 @@ from pyparsing import __version__ as pyparsing_version
 from pyparsing import ( nestedExpr, Literal, CaselessLiteral, Word, OneOrMore, ZeroOrMore,
     Forward, NotAny, delimitedList, oneOf, Group, Optional, Combine, alphas, nums,
     restOfLine, cStyleComment, nums, alphanums, printables, empty, quotedString,
-    ParseException, ParseResults, CharsNotIn, _noncomma, dblQuotedString, QuotedString, ParserElement )
+    ParseException, ParseResults, CharsNotIn, dblQuotedString, QuotedString, ParserElement )
 
 
 class P_AttrList:
@@ -414,7 +414,8 @@ def graph_definition():
 
         double_quoted_string = QuotedString('"', multiline=True, unquoteResults=False) # dblQuotedString
 
-        alphastring_ = OneOrMore(CharsNotIn(_noncomma + ' '))
+        noncomma = "".join([c for c in printables if c != ","])
+        alphastring_ = OneOrMore(CharsNotIn(noncomma + ' '))
 
         def parse_html(s, loc, toks):
             return '<%s>' % ''.join(toks[0])
