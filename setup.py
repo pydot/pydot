16a9eeddaaa56798b72bb77ac7b1935b80255f72
Ioannis Filippidis <jfilippidis@gmail.com>
2016-06-28 21:04:21 -0700

    PEP8: `autopep8 -i setup.py`

--- setup.py
+++ setup.py
@@ -7,17 +7,19 @@ except ImportError:
 import pydot
 
 
-setup(	name = 'pydot',
-    version = pydot.__version__,
-    description = 'Python interface to Graphviz\'s Dot',
-    author = 'Ero Carrera',
-    author_email = 'ero@dkbza.org',
-    maintainer = 'Ioannis Filippidis',
-    maintainer_email = 'jfilippidis@gmail.com',
-    url = 'https://github.com/erocarrera/pydot',
-    license = 'MIT',
-    keywords = 'graphviz dot graphs visualization',
-    classifiers =	['Development Status :: 5 - Production/Stable',
+setup(
+    name='pydot',
+    version=pydot.__version__,
+    description='Python interface to Graphviz\'s Dot',
+    author='Ero Carrera',
+    author_email='ero@dkbza.org',
+    maintainer='Ioannis Filippidis',
+    maintainer_email='jfilippidis@gmail.com',
+    url='https://github.com/erocarrera/pydot',
+    license='MIT',
+    keywords='graphviz dot graphs visualization',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
@@ -26,7 +28,7 @@ setup(	name = 'pydot',
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering :: Visualization',
         'Topic :: Software Development :: Libraries :: Python Modules'],
-    long_description = "\n".join(pydot.__doc__.split('\n')),
-    py_modules = ['pydot', 'dot_parser'],
-    install_requires = ['pyparsing>=1.5.7'],
-    data_files = [('.', ['LICENSE', 'README'])] )
+    long_description="\n".join(pydot.__doc__.split('\n')),
+    py_modules=['pydot', 'dot_parser'],
+    install_requires=['pyparsing>=1.5.7'],
+    data_files=[('.', ['LICENSE', 'README'])])
