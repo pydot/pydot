e4173042ec85d5fc02ac8dd80b4160264968108b
Ioannis Filippidis <jfilippidis@gmail.com>
2016-06-30 23:31:10 -0700

    BUG: correct test that depends on enumeration order of `dict`
    Alternatively, the attribute values can be sorted within the
    method `pydot.Node.to_string`. However, there is little reason
    to introduce sorting, and for efficiency reasons better avoid it.

--- test/pydot_unittest.py
+++ test/pydot_unittest.py
@@ -65,11 +65,11 @@ class TestGraphAPI(unittest.TestCase):
         node = pydot.Node('legend')
         node.set("shape", 'box')
         g.add_node(node)
-        node.set('label','mine')
-
-        self.assertEqual(g.to_string(),
-                         'digraph G {\nlegend [shape=box, label=mine];\n}\n')
-
+        node.set('label', 'mine')
+        s = g.to_string()
+        s_0 = 'digraph G {\nlegend [label=mine, shape=box];\n}\n'
+        s_1 = 'digraph G {\nlegend [shape=box, label=mine];\n}\n'
+        assert s == s_0 or s == s_1, (s, s_0)
 
     def test_attribute_with_implicit_value(self):
 
