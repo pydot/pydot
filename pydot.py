245a684dda1c55fcb89198cbefe7ba80445b4841
Ioannis Filippidis <jfilippidis@gmail.com>
2016-07-11 01:02:39 -0700

    DOC: add docstring to function `quote_if_necessary`

--- pydot.py
+++ pydot.py
@@ -200,6 +200,7 @@ def needs_quotes( s ):
 
 
 def quote_if_necessary(s):
+    """Enclode attribute value in quotes, if needed."""
     if isinstance(s, bool):
         if s is True:
             return 'True'
