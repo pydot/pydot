526f1386eeb253c8d581a2ddb4c1f3602cd50d30
Sebastian Kalinowski <sebastian@kalinowski.eu>
2017-10-23 20:15:42 +0200

    BUG: mv package description to within `setup.py`
    Reported in #145.

--- pydot.py
+++ pydot.py
@@ -1,20 +1,4 @@
-"""Graphviz's dot language Python interface.
-
-This module provides with a full interface to
-create handle modify and process graphs in
-Graphviz's dot language.
-
-References:
-
-pydot Homepage: https://github.com/erocarrera/pydot
-Graphviz:       http://www.graphviz.org/
-DOT Language:   http://www.graphviz.org/doc/info/lang.html
-
-Copyright (c) 2005-2011 Ero Carrera <ero.carrera@gmail.com>
-
-Distributed under MIT license
-[http://opensource.org/licenses/mit-license.html].
-"""
+"""An interface to GraphViz."""
 from __future__ import division
 from __future__ import print_function
 import copy
