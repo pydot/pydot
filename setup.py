2b651c7d1bfc6c0508d03efc96ae41e5d48676f1
Sebastian Kalinowski <sebastian@kalinowski.eu>
2017-10-23 22:06:28 -0700

    DOC: list reference links within `long_description`
    to ensure the desired formatting on PyPI.

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ Graphviz's dot language.
 
 References:
 
-pydot Homepage: https://github.com/erocarrera/pydot
-Graphviz:       http://www.graphviz.org/
-DOT Language:   http://www.graphviz.org/doc/info/lang.html
+* pydot Homepage: https://github.com/erocarrera/pydot
+* Graphviz:       http://www.graphviz.org/
+* DOT Language:   http://www.graphviz.org/doc/info/lang.html
 
 Copyright (c) 2005-2011 Ero Carrera <ero.carrera@gmail.com>
 
