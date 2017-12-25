c46584ca838e3368413ccaae2f4dba49060f2e1f
Ioannis Filippidis <jfilippidis@gmail.com>
2017-12-25 01:04:33 -0800

    REL: add Trove classifier for Python 3.7

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: Visualization',
         'Topic :: Software Development :: Libraries :: Python Modules'],
     long_description=long_description,
