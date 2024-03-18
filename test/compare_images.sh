fc934ea4aa66cf2f9e9ac530e3626b1a777f73e5
Frank Dana <ferdnyc@gmail.com>
2024-03-18 10:12:07 -0400

    Enhance compare_images.py with animation (#329)
    - In addition to the image-difference comparison already implemented,
      have `compare_images.sh` (which is included in downloads of comparison
      error archives from the CI system, and only run by the downloader)
      also create a looping animated GIF showing the two images one after
      the other, which is turning out to be a superior comparison method.

--- test/compare_images.sh
+++ test/compare_images.sh
@@ -4,4 +4,7 @@ for dir in */; do
     magick composite $dir/err_pydot.jpeg \
         -compose difference $dir/err_graphviz.jpeg \
         $dir/err_difference.png
+    magick convert -delay 100 $dir/err_pydot.jpeg \
+	-delay 100 $dir/err_graphviz.jpeg \
+	$dir/anim_difference.gif
 done
