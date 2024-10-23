2ee7c2d6cbe3fe965118db3d98e69d0721dd7ebd
lkk7 <lukaszlapinski7@gmail.com>
2024-10-23 19:27:48 +0200

    Add initial type annotations

--- src/pydot/classes.py
+++ src/pydot/classes.py
@@ -5,9 +5,10 @@
 """Frozen dictionaries."""
 
 import copy
+from typing import Any, Tuple
 
 
-class FrozenDict(dict):
+class FrozenDict(dict):  # type: ignore
     """Frozen dictionary, values are immutable after creation.
 
     Extended version of ASPN's Python Cookbook Recipe:
@@ -17,29 +18,29 @@ class FrozenDict(dict):
 
     _block_msg = "A frozendict cannot be modified."
 
-    def __delitem__(self, key):
+    def __delitem__(self, key: Any) -> None:
         raise AttributeError(self._block_msg)
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: Any, value: Any) -> None:
         raise AttributeError(self._block_msg)
 
-    def clear(self):
+    def clear(self) -> None:
         raise AttributeError(self._block_msg)
 
-    def pop(self, key, default=None):
+    def pop(self, key: Any, default: Any = None) -> None:
         raise AttributeError(self._block_msg)
 
-    def popitem(self):
+    def popitem(self) -> Tuple[Any, Any]:
         raise AttributeError(self._block_msg)
 
-    def setdefault(self, key, default=None):
+    def setdefault(self, key: Any, default: Any = None) -> None:
         raise AttributeError(self._block_msg)
 
-    def update(self, *E, **F):
+    def update(self, *E: Any, **F: Any) -> None:
         raise AttributeError(self._block_msg)
 
     @staticmethod
-    def _freeze_arg(in_arg):
+    def _freeze_arg(in_arg: Any) -> Any:
         if not isinstance(in_arg, dict):
             return in_arg
         arg = copy.copy(in_arg)
@@ -54,30 +55,30 @@ class FrozenDict(dict):
                 )
         return arg
 
-    def __new__(cls, *args, **kw):
+    def __new__(cls, *args: Any, **kw: Any) -> Any:
         new = dict.__new__(cls)
         args_ = [cls._freeze_arg(arg) for arg in args]
         dict.__init__(new, *args_, **cls._freeze_arg(kw))
         return new
 
-    def __init__(self, *args, **kw):
+    def __init__(self, *args: Any, **kw: Any):
         pass
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         if isinstance(other, self.__class__):
             return hash(self) == hash(other)
         return NotImplemented
 
-    def __ne__(self, other):
+    def __ne__(self, other: Any) -> bool:
         return not self == other
 
-    def __hash__(self):
+    def __hash__(self) -> int:  # type: ignore
         try:
-            return self._cached_hash
+            return self._cached_hash  # type: ignore
         except AttributeError:
             self._cached_hash = hash(tuple(self.items()))
             return self._cached_hash
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         dict_repr = dict.__repr__(self)
         return f"FrozenDict({dict_repr})"
