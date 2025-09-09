78086d8e0595dc41251047af38f2c78193ba412e
FeRD (Frank Dana) <ferdnyc@gmail.com>
2025-09-09 18:25:15 -0400

    Tests: Clean up test_pydot.py
    - Add a bunch of type annotations and defensive coding to eliminate
      red squiggles.
    - Use `# pragma: no cover` and `# pragma: no branch` comments
      to exclude uncovered code that only runs when tests fail.
      (If those lines _do_ have coverage, it's a bad thing!)
    - Remove some inheritable methods of the RenderResult subclasses

--- test/test_pydot.py
+++ test/test_pydot.py
@@ -33,30 +33,35 @@ _test_root = os.path.dirname(os.path.abspath(__file__))
 class RenderResult:
     """Results object returned by Renderer methods."""
 
-    def __init__(self, data, dot_file=None, dot_src=None) -> None:
+    def __init__(
+        self,
+        data: bytes,
+        dot_file: str | None = None,
+        dot_src: str | None = None,
+    ) -> None:
         self._data = data
         self._dot_file: T.Optional[str] = dot_file
         self._dot_src: T.Optional[str] = dot_src
 
     @property
-    def data(self):
+    def data(self) -> bytes:
         """Get the raw image data for the result."""
         return self._data
 
     @property
-    def dot_file(self):
+    def dot_file(self) -> str:
         """Get the graph data used to generate the file."""
-        return self._dot_file
+        return self._dot_file or ""  # pragma: no cover
 
     @property
-    def dot_src(self):
-        if self._dot_src is None and self._dot_file is not None:
-            with open(self._dot_file, encoding="utf-8") as _in:
+    def dot_src(self) -> str:  # pragma: no cover
+        if self._dot_src is None and self.dot_file is not None:
+            with open(self.dot_file, encoding="utf-8") as _in:
                 self._dot_src = _in.read()
-        return self._dot_src
+        return self._dot_src or ""
 
     @functools.cached_property
-    def checksum(self):
+    def checksum(self) -> str:
         """Get the sha256 checksum for the result."""
         return sha256(self.data).hexdigest()
 
@@ -67,20 +72,9 @@ class GVRenderResult(RenderResult):
     Unlike RenderResult, GVRenderResult will always have a dot_file.
     """
 
-    def __init__(self, data, dot_file: str) -> None:
+    def __init__(self, data: bytes, dot_file: str) -> None:
         super().__init__(data)
         self._dot_file: str = dot_file
-
-    @property
-    def dot_file(self):
-        return self._dot_file
-
-    @property
-    def dot_src(self):
-        if self._dot_src is None:
-            with open(self._dot_file, encoding="utf-8") as _in:
-                self._dot_src = _in.read()
-        return self._dot_src
 
 
 class PydotRenderResult(RenderResult):
@@ -89,20 +83,16 @@ class PydotRenderResult(RenderResult):
     Unlike RenderResult, PydotRenderResult will always have a dot_src.
     """
 
-    def __init__(self, data, dot_src: str) -> None:
+    def __init__(self, data: bytes, dot_src: str) -> None:
         super().__init__(data)
         self._dot_src: str = dot_src
-
-    @property
-    def dot_src(self):
-        return self._dot_src
 
 
 class Renderer:
     """Call pydot renderers for data files."""
 
     @classmethod
-    def graphviz(cls, filename, encoding):
+    def graphviz(cls, filename: str, encoding: str) -> GVRenderResult:
         with open(filename, encoding=encoding) as stdin:
             stdout_data, stderr_data, process = pydot.call_graphviz(
                 program=TEST_PROGRAM,
@@ -114,9 +104,9 @@ class Renderer:
         return GVRenderResult(stdout_data, filename)
 
     @classmethod
-    def pydot(cls, filename, encoding):
+    def pydot(cls, filename: str, encoding: str) -> PydotRenderResult:
         c = pydot.graph_from_dot_file(filename, encoding=encoding)
-        if not c:
+        if not c:  # pragma: no cover
             raise RuntimeError("No data returned from pydot!")
         jpe_data = bytearray()
         for g in c:
@@ -127,19 +117,18 @@ class Renderer:
         return PydotRenderResult(jpe_data, src)
 
 
-def _load_test_cases(casedir):
+def _load_test_cases(casedir: str) -> list[tuple[str, str, str]]:
     """Return a list of testcase files,
 
     Returns a list of tuples of the form:
         ("case_file_name", "case_file_name.dot", "path/to/directory")
     """
-    global _test_root
     path = os.path.join(_test_root, casedir)
     dot_files = filter(lambda x: x.endswith(".dot"), os.listdir(path))
 
     def _case_name(fname: str) -> str:
         """No str.removesuffix() until Python 3.9."""
-        if sys.version_info < (3, 9):
+        if sys.version_info < (3, 9):  # pragma: no cover
             return fname
         return fname.removesuffix(".dot")
 
@@ -155,7 +144,7 @@ def _compare_images(
     a subdir for the test and dump both images there for examination."""
     if pydot.checksum == gv.checksum:
         return True
-    if TEST_ERROR_DIR is not None:
+    if TEST_ERROR_DIR is not None:  # pragma: no cover
         dirname = fname.replace(".", "_")
         out_dir = os.path.join(os.path.normpath(TEST_ERROR_DIR), dirname)
         os.makedirs(out_dir)
@@ -170,14 +159,14 @@ def _compare_images(
         with open(gv_src, "w") as gs, open(pd_src, "w") as ps:
             ps.write(pydot.dot_src)
             gs.write(gv.dot_src)
-    return False
+    return False  # pragma: no cover
 
 
 class PydotTestCase(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self._reset_graphs()
 
-    def _reset_graphs(self):
+    def _reset_graphs(self) -> None:
         self.graph_directed = pydot.Graph("testgraph", graph_type="digraph")
 
 
@@ -185,12 +174,12 @@ class TestShapeFiles(PydotTestCase):
     shapefile_dir = os.path.join(_test_root, "from-past-to-future")
 
     # image files are omitted from sdist
-    @unittest.skipUnless(
+    @unittest.skipUnless(  # pragma: no cover
         os.path.isdir(shapefile_dir),
         "Skipping tests that involve images,"
         + " they can be found in the git repository",
     )
-    def test_graph_with_shapefiles(self):
+    def test_graph_with_shapefiles(self) -> None:
         dot_file = os.path.join(self.shapefile_dir, "from-past-to-future.dot")
 
         pngs = [
@@ -199,7 +188,7 @@ class TestShapeFiles(PydotTestCase):
             if fname.endswith(".png")
         ]
 
-        with open(dot_file) as f:
+        with open(dot_file, encoding="utf-8") as f:
             graph_data = f.read()
 
         graphs = pydot.graph_from_dot_data(graph_data)
@@ -221,14 +210,14 @@ class TestShapeFiles(PydotTestCase):
 
 
 class RenderedTestCase(PydotTestCase):
-    def _render_and_compare_dot_file(self, fdir, fname):
+    def _render_and_compare_dot_file(self, fdir: str, fname: str) -> None:
         # files that confuse `chardet`
         encodings = {"Latin1.dot": "latin-1"}
         fpath = os.path.join(fdir, fname)
         with open(fpath, "rb") as f:
             s = f.read()
-        estimate = chardet.detect(s)
-        encoding = encodings.get(fname, estimate["encoding"])
+        estimate: str = chardet.detect(s).get("encoding") or "utf-8"
+        encoding: str = encodings.get(fname, estimate)
         rendered = Renderer.pydot(
             fpath,
             encoding,
@@ -237,7 +226,8 @@ class RenderedTestCase(PydotTestCase):
             fpath,
             encoding,
         )
-        if not _compare_images(fname, rendered, graphviz):
+        output_matches = _compare_images(fname, rendered, graphviz)
+        if not output_matches:  # pragma: no cover
             raise AssertionError(
                 f"{fname}: {rendered.checksum} != {graphviz.checksum} "
                 "(found pydot vs graphviz difference)"
@@ -248,7 +238,7 @@ class TestMyRegressions(RenderedTestCase):
     """Perform regression tests in my_tests dir."""
 
     @parameterized.expand(functools.partial(_load_test_cases, TESTS_DIR_1))
-    def test_regression(self, _, fname, path):
+    def test_regression(self, _: T.Never, fname: str, path: str) -> None:
         self._render_and_compare_dot_file(path, fname)
 
 
@@ -256,5 +246,5 @@ class TestGraphvizRegressions(RenderedTestCase):
     """Perform regression tests in graphs dir."""
 
     @parameterized.expand(functools.partial(_load_test_cases, TESTS_DIR_2))
-    def test_regression(self, _, fname, path):
+    def test_regression(self, _: T.Never, fname: str, path: str) -> None:
         self._render_and_compare_dot_file(path, fname)
